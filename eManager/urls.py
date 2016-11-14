ea849527a32311382f0341a894d0f806e66d49da
BEGEMOT9I <daston2012@gmail.com>
2016-11-14 19:32:52 +0300

    added path to uploaded content

--- eManager/urls.py
+++ eManager/urls.py
@@ -6,5 +6,6 @@ from . import views
 app_name = 'emanager'
 
 urlpatterns = [
+	url(r'^$', views.MainView.as_view(), name='index'),
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
 ]
