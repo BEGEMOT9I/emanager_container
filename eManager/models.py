fc8a8d3497fab8d14178ef0fab7ad5787f4fadee
System Administrator <root@MacBook-Pro-Dara.local>
2016-11-14 05:09:39 +0300

    Changed model Event

--- eManager/models.py
+++ eManager/models.py
@@ -10,10 +10,13 @@ from django.utils import timezone
 @python_2_unicode_compatible
 class Event(models.Model):
 	
+	city = models.CharField(max_length=50, verbose_name='город', default='')
 	organizer = models.CharField(max_length=200, verbose_name='организатор', default='')
 	name = models.CharField(max_length=1000, verbose_name='название')
 	start_date = models.DateTimeField(verbose_name='дата начала')
 	description = models.CharField(max_length=1000, verbose_name='описание')
+	addres = models.CharField(max_length=200, verbose_name='место проведения', default='')
+	image = models.ImageField(upload_to='images', blank=True, verbose_name='постер')
 
 	class Meta:
 		verbose_name = 'событие'
