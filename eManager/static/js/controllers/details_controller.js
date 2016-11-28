16c6cc1b2467ad386b6dfbe5cd542a9ca17e7f2d
BEGEMOT9I <daston2012@gmail.com>
2016-11-28 19:06:43 +0300

    details controller

---
+++ eManager/static/js/controllers/details_controller.js
@@ -0,0 +1,211 @@
+window.DetailsController = function() {
+	var mapStyle =
+	[
+		{
+			"featureType": "water",
+			"elementType": "geometry",
+			"stylers": [
+				{
+					"color": "#e9e9e9"
+				},
+				{
+					"lightness": 17
+				}
+			]
+		},
+		{
+			"featureType": "landscape",
+			"elementType": "geometry",
+			"stylers": [
+				{
+					"color": "#f5f5f5"
+				},
+				{
+					"lightness": 20
+				}
+			]
+		},
+		{
+			"featureType": "road.highway",
+			"elementType": "geometry.fill",
+			"stylers": [
+				{
+					"color": "#ffffff"
+				},
+				{
+					"lightness": 17
+				}
+			]
+		},
+		{
+			"featureType": "road.highway",
+			"elementType": "geometry.stroke",
+			"stylers": [
+				{
+					"color": "#ffffff"
+				},
+				{
+					"lightness": 29
+				},
+				{
+					"weight": 0.2
+				}
+			]
+		},
+		{
+			"featureType": "road.arterial",
+			"elementType": "geometry",
+			"stylers": [
+				{
+					"color": "#ffffff"
+				},
+				{
+					"lightness": 18
+				}
+			]
+		},
+		{
+			"featureType": "road.local",
+			"elementType": "geometry",
+			"stylers": [
+				{
+					"color": "#ffffff"
+				},
+				{
+					"lightness": 16
+				}
+			]
+		},
+		{
+			"featureType": "poi",
+			"elementType": "geometry",
+			"stylers": [
+				{
+					"color": "#f5f5f5"
+				},
+				{
+					"lightness": 21
+				}
+			]
+		},
+		{
+			"featureType": "poi.park",
+			"elementType": "geometry",
+			"stylers": [
+				{
+					"color": "#dedede"
+				},
+				{
+					"lightness": 21
+				}
+			]
+		},
+		{
+			"elementType": "labels.text.stroke",
+			"stylers": [
+				{
+					"visibility": "on"
+				},
+				{
+					"color": "#ffffff"
+				},
+				{
+					"lightness": 16
+				}
+			]
+		},
+		{
+			"elementType": "labels.text.fill",
+			"stylers": [
+				{
+					"saturation": 36
+				},
+				{
+					"color": "#333333"
+				},
+				{
+					"lightness": 40
+				}
+			]
+		},
+		{
+			"elementType": "labels.icon",
+			"stylers": [
+				{
+					"visibility": "off"
+				}
+			]
+		},
+		{
+			"featureType": "transit",
+			"elementType": "geometry",
+			"stylers": [
+				{
+					"color": "#f2f2f2"
+				},
+				{
+					"lightness": 19
+				}
+			]
+		},
+		{
+			"featureType": "administrative",
+			"elementType": "geometry.fill",
+			"stylers": [
+				{
+					"color": "#fefefe"
+				},
+				{
+					"lightness": 20
+				}
+			]
+		},
+		{
+			"featureType": "administrative",
+			"elementType": "geometry.stroke",
+			"stylers": [
+				{
+					"color": "#fefefe"
+				},
+				{
+					"lightness": 17
+				},
+				{
+					"weight": 1.2
+				}
+			]
+		}
+	];
+
+	var address = document.getElementById("event__map").getAttribute('data-position'),
+		geocoder = new google.maps.Geocoder();
+
+	var mapLatlng = new google.maps.LatLng(59.941342, 30.370493);
+
+	var mapOptions = {
+		zoom: 16,
+		styles: mapStyle,
+		center: mapLatlng,
+		disableDefaultUI: true
+	}
+
+	var map = new google.maps.Map(document.getElementById("event__map"), mapOptions);
+
+	geocoder.geocode( { 'address': address}, function(results, status) {
+		if (status == google.maps.GeocoderStatus.OK) {
+			map.setCenter(results[0].geometry.location);
+			var marker = new google.maps.Marker({
+				map: map,
+				animation: google.maps.Animation.DROP,
+				position: results[0].geometry.location,
+				// icon: {
+				//	 url: '/marker.png',
+				//	 size: new google.maps.Size(56, 75)
+				// },
+				title: "Место проведения"
+			});
+		} else {
+			alert("Geocode was not successful for the following reason: " + status);
+		}
+	});
+}
